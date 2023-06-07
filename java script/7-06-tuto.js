ae2c7fe899dae579b2ef33415125131caacc95c6
Dilip Varma <vddilipvarma@gmail.com>
2023-06-07 23:46:56 +0530

    portfolio

---
+++ java script/7-06-tuto.js
@@ -0,0 +1,11 @@
+console.log(a);
+var a=30;
+console.log();
+const c=30;
+function call(a,b,c){
+var result=a+b+c;
+return result;
+}
+//console.log(result);
+let result=call(10,20,30);
+console.log(result);

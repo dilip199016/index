88d2aaf2a1f6d5feced75591e1344cf77ac7fcb0
Dilip Varma <vddilipvarma@gmail.com>
2023-07-27 20:44:37 +0530

    button changes

--- java script/Function.js
+++ java script/Function.js
@@ -1,1 +1,21 @@
 console.log("Today class is About Function");
+
+
+
+function isNumber(value){
+if(isNaN(value)){
+    return false;
+}
+else if(value){
+    return true;
+}
+else{
+    return false;
+}
+}
+function isArray(value) {
+    return Array.isArray(value);
+  }
+console.log(isNumber("Dilip"));
+console.log(isNumber(""));
+console.log(isNumber(1234));

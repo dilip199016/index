5cb6d2b0ae3bfe8d322bc8a33f42a60f5e366baa
Dilip Varma <vddilipvarma@gmail.com>
2023-08-02 23:06:21 +0530

    inheritance

---
+++ java script/Inheritance.js
@@ -0,0 +1,38 @@
+const employee = {
+    name: "Vishal",
+  };
+  
+  console.log(employee);
+
+
+  const stud={
+    collega:"Newton",
+    City:"Benglore",
+  };
+  const stud1={
+Name:"Dilip varma",
+  };
+  Object.setPrototypeOf(stud,stud1);
+  console.log("stud1", Object.getPrototypeOf(stud));
+
+
+  let animal12 = {
+    walk() {
+      console.log("I can walk");
+      console.log("animal12", this);
+    },
+    eat() {
+      console.log("I can walk");
+    },
+  };
+  
+  const cow123 = {
+    name: "apni gaye",
+    callMe() {
+      console.log("cow", this);
+    },
+    __proto__: animal12,
+  };
+  
+  cow123.callMe(); // cow
+  cow123.walk(); //
